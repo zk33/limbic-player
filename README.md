29b1a1d12d3494d5a024c51e3d1405868eef74b0
zk33 <ymzktkak@gmail.com>
2013-12-24 20:59:39 +0900

    Initialize repositry using gurnt-init jquery

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-limbic-player
-=============
+# Limbic Player
 
-content player for limbic.cc
+multi content player for limbic.cc
+
+## Getting Started
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/zk33/limbic-player/master/dist/limbic-player.min.js
+[max]: https://raw.github.com/zk33/limbic-player/master/dist/limbic-player.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/limbic-player.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ src/limbic-player.js
@@ -0,0 +1,38 @@
+/*
+ * limbic-player
+ * https://github.com/zk33/limbic-player
+ *
+ * Copyright (c) 2013 zk33
+ * Licensed under the MIT license.
+ */
+
+(function($) {
+
+  // Collection method.
+  $.fn.limbic_player = function() {
+    return this.each(function(i) {
+      // Do something awesome to each selected element.
+      $(this).html('awesome' + i);
+    });
+  };
+
+  // Static method.
+  $.limbic_player = function(options) {
+    // Override default options with passed-in options.
+    options = $.extend({}, $.limbic_player.options, options);
+    // Return something awesome.
+    return 'awesome' + options.punctuation;
+  };
+
+  // Static method default options.
+  $.limbic_player.options = {
+    punctuation: '.'
+  };
+
+  // Custom selector.
+  $.expr[':'].limbic_player = function(elem) {
+    // Is this element awesome?
+    return $(elem).text().indexOf('awesome') !== -1;
+  };
+
+}(jQuery));
